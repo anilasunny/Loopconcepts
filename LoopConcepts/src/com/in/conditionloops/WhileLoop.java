439378629cdfca4562831dbe4e88df167ef89dc8
Anila Sunny <c0842969@mylambton.ca>
2024-07-04 16:12:52 -0400

    Second commit

---
+++ LoopConcepts/src/com/in/conditionloops/WhileLoop.java
@@ -0,0 +1,22 @@
+package com.in.conditionloops;
+
+public class WhileLoop {
+	
+	public void whileLoop() {
+		
+		int a=0;
+		while(a<10) {
+			System.out.println("Number is " +a);
+			a=a+1;
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhileLoop obj1 =new WhileLoop();
+		obj1.whileLoop();
+	}
+
+}
